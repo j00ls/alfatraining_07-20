95cdce9a32de819944ef2956aaf7f1830183797e
Manfred Wisniewski <manfred@witconsult.de>
2020-06-29 15:36:04 +0200

    WIP - basic setup for dateDiffString function

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 var cowsay = require("cowsay");
 
-console.log(cowsay.say({
+console.log(cowsay.think({
 	text : "I'm a moooodule",
 	e : "oO",
 	T : "U "
 }));
+
+
+date1 = randomDate(new Date(2020, 0, 1), new Date())
+date2 = randomDate(new Date(2020, 0, 1), new Date())
+
+function dateDiffString() {
+  //TODO implement
+}
+
+function randomDate(start, end) {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+}
+
+function dateDiffString() {
+  console.log(date1);
+  console.log(date2);
+}
