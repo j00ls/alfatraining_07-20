4ce81604016d043a7381b04de435c8c7b45f1871
Manfred Wisniewski <manfred@witconsult.de>
2020-06-29 20:12:49 +0200

    added calculate time difference

--- index.js
+++ index.js
@@ -10,15 +10,16 @@ console.log(cowsay.think({
 date1 = randomDate(new Date(2020, 0, 1), new Date())
 date2 = randomDate(new Date(2020, 0, 1), new Date())
 
-function dateDiffString() {
-  //TODO implement
-}
+dateDiffString();
+
 
 function randomDate(start, end) {
     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
 }
 
 function dateDiffString() {
-  console.log(date1);
-  console.log(date2);
+	//TODO implement
+  console.log("date1: " + date1);
+  console.log("date2: " + date2);
+	console.log("time difference: " + (date1 - date2)/(1000 * 3600 * 24) + " days");
 }
